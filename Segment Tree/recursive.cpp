507bfcb5946823c3554095e6094a755566512148
Wayne716 <waynewood@hotmail.com>
2020-05-26 11:14:48 +0800

    Update recursive.cpp

--- Segment Tree/recursive.cpp
+++ Segment Tree/recursive.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 #define MAX_LEN 1000
 
-void build_tree(int arr[], int tree[], int node, int start, int end) {
+void build_tree(int arr[], int start, int end, int tree[], int node) {
     if (start == end)
         tree[node] = arr[start];
     else {
         int mid = (start + end) / 2;
-        int left_node  = 2 * node + 1;
-        int right_node = 2 * node + 2;
-        build_tree(arr, tree, left_node, start, mid);
-        build_tree(arr, tree, right_node, mid+1, end);
+        int left_node  = node * 2 + 1;
+        int right_node = node * 2 + 2;
+        build_tree(arr, start, mid, tree, left_node);
+        build_tree(arr, mid+1, end, tree, right_node);
         tree[node] = tree[left_node] + tree[right_node];
     }
 }
 
-void update_tree(int arr[], int tree[], int node, int start, int end, int idx, int val) {
+void update_tree(int arr[], int start, int end, int tree[], int node, int idx, int val) {
     if (start == end) {
         arr[idx] = val;
         tree[node] = val;
@@ -25,36 +25,37 @@ void update_tree(int arr[], int tree[], int node, int start, int end, int idx, i
         int mid = (start + end) / 2;
         int left_node  = node * 2 + 1;
         int right_node = node * 2 + 2;
-        if (idx >= start && idx <= mid)
-            update_tree(arr, tree, left_node, start, mid, idx, val);
+        if (start <= idx && idx <= mid)
+            update_tree(arr, start, mid, tree, left_node, idx, val);
         else
-            update_tree(arr, tree, right_node, mid+1, end, idx, val);
+            update_tree(arr, mid+1, end, tree, right_node, idx, val);
         tree[node] = tree[left_node] + tree[right_node];
     }
 }
 
-int query_tree(int arr[], int tree[], int node, int start, int end, int L, int R) {
+int query_tree(int arr[], int start, int end, int tree[], int node, int L, int R) {
     if (R < start || L > end)
         return 0;
-    else if ((end <= R && start >= L) || start == end)
+    else if ((start >= L && end <= R) || start == end)
         return tree[node];
     else {
         int mid = (start + end) / 2;
         int left_node  = node * 2 + 1;
         int right_node = node * 2 + 2;
-        int sum_left  = query_tree(arr, tree, left_node, start, mid, L, R);
-        int sum_right = query_tree(arr, tree, right_node, mid + 1, end, L, R);
-        return sum_left + sum_right;
+        int left_sum  = query_tree(arr, start, mid, tree, left_node, L, R);
+        int right_sum = query_tree(arr, mid+1, end, tree, right_node, L, R);
+        return left_sum + right_sum;
     }
 }
 
-int main()
-{
-    int arr[] = {1,3,5,7,9,11};
-    int size = 6;
+int main() {
+    int arr[] = {1, 3, 5, 7, 9, 11};
     int tree[MAX_LEN] = {0};
-    build_tree(arr, tree, 0, 0, size-1);
-    update_tree(arr, tree, 0, 0, size-1, 4, 6);
-    cout << query_tree(arr, tree, 0, 0, size-1, 2, 5);
+    int size = 6;
+    build_tree(arr, 0, size-1, tree, 0);
+    update_tree(arr, 0, size-1, tree, 0, 4, 6);
+    for (int i=0; i<14; ++i)
+        cout << tree[i] << ' ';
+    cout << query_tree(arr, 0, size-1, tree, 0, 2, 5);
     return 0;
 }
